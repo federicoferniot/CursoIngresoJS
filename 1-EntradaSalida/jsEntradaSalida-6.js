1d488348d318b2bfbbbf6e61918adff0d53ae827
federicoferniot <36281586+federicoferniot@users.noreply.github.com>
2018-02-08 22:12:48 -0300

    Ejercicio 6
    Convertir a entero y sumarlos.

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	//Convierto a enteros
+	numeroUno = parseInt(document.getElementById('numeroUno').value);
+	numeroDos = parseInt(document.getElementById('numeroDos').value);
+
+	//Sumo enteros
+	resultado = numeroUno + numeroDos;
+
+	alert("la suma es " + resultado);
 }
 
