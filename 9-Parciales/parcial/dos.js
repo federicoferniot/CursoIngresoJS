c464b0b271e79af506c35f0c432edea59b50696a
federicoferniot <36281586+federicoferniot@users.noreply.github.com>
2018-02-09 21:32:21 -0300

    Parcial 1 - Ejercicio 1 y 2

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,10 @@
 function Mostrar()
 {
-  
+  var importe;
+  var importeFinal;
+
+  importe = parseInt(prompt("Ingrese el importe"));
+
+  importeFinal = importe + (importe*21/100);
+  document.getElementById('importeFinal').value = importeFinal;
 }
