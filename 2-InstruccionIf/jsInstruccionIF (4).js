94fc527b276b8e945b59da8b53b0bee02faca34a
federicoferniot <36281586+federicoferniot@users.noreply.github.com>
2018-02-09 22:14:06 -0300

    Ejercicios 4 y 5
    Operadores lógicos

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,13 @@
 function Mostrar()
 {
+	var edad;
 //tomo la edad  
-
+	edad = document.getElementById('edad').value;
+	
+	if((edad>=13) && (edad<=17))
+	{
+		alert("Es adolescente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
