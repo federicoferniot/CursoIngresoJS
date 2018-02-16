0062c86cc54e8df2621af56ede2626cc7b7c574a
federicoferniot <36281586+federicoferniot@users.noreply.github.com>
2018-02-16 19:31:25 -0300

    Ejercicios 3 y 4
    If

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,26 @@
 function Mostrar()
 {
+	var numeroUno, numeroDos, resultado;
 
+	numeroUno = parseInt(prompt("Ingrese el primer número"));
+	numeroDos = parseInt(prompt("Ingrese el segundo número"));
+
+	if(numeroUno == numeroDos)
+	{
+		resultado = numeroUno*numeroDos;
+		//document.write(numeroUno*numeroDos);
+	}
+	else if(numeroUno > numeroDos)
+	{
+		resultado = numeroUno-numeroDos;
+		//document.write(numeroUno-numeroDos);
+	}
+	else
+	{
+		resultado = numeroUno+numeroDos;
+		//document.write(numeroUno+numeroDos);
+	}
+
+	//Escribo el resultado
+	document.write(resultado);
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,11 @@
 function Mostrar()
 {
+	var largo, ancho, perimetro;
 
+	largo = parseInt(document.getElementById('alrgo').value);
+	ancho = parseInt(document.getElementById('ancho').value);
+
+	perimetro = (largo*2)+(ancho*2);
+
+	alert("Se necesitan "+(perimetro*3)+"m de alambre");
 }
