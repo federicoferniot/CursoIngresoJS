bcb770d6d45be027375c6691da74ebda5b4577af
federicoferniot <36281586+federicoferniot@users.noreply.github.com>
2018-02-16 20:43:20 -0300

    Ejercicio 5 - Adivina el número
    Random, If, else if.

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -8,19 +8,56 @@ secreto del 1 al 100, en la pantalla del juego
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
 var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos=0;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+	numeroSecreto = Math.floor(Math.random() * 100)+1;
 		//alert(numeroSecreto );
-	
+	console.log(numeroSecreto);
+	contadorIntentos=0;
+	document.getElementById('intentos').value = contadorIntentos;
 
 }
 
 function verificar()
 {
-	
+	var numeroIngresado;
+
+	/* Contador, otra formas de realizarlo:
+		contadorIntetos = contadoIntentos + 1;
+		contadorIntentos += 1;
+		++contador; (pre incremento)
+	*/
+	contadorIntentos++; //post incremento
+	document.getElementById('intentos').value = contadorIntentos;
+
+	if(contadorIntentos>10)
+	{
+		alert("Perdiste");
+		return;
+	}
+
+	numeroIngresado = document.getElementById('numero').value;
+	if(numeroIngresado<numeroSecreto)
+	{
+		alert("falta...");
+	}
+	else if(numeroIngresado>numeroSecreto)
+	{
+		alert("se pasó...");
+	}
+	else if(contadorIntentos!=1)
+	{
+		alert("Usted es un ganador!!! y en solo "+contadorIntentos+" intentos");
+	}
+	else
+	{
+		numeroSecreto = Math.floor(Math.random() * 100)+1;
+		console.log(numeroSecreto);
+		contadorIntentos=0;
+		verificar();
+	}
 	
 }
